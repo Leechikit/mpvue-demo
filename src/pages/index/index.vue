c0025595321e96982b50387e790e9e54aa72574e
leechikit <leechikit0823@163.com>
2019-06-13 17:43:22 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,45 +1,24 @@
 <template>
-  <div class="container" @click="clickHandle">
-    <div class="message">{{msg}}</div>
-    <!-- 创建 3个 click-counter 组件 -->
-    <click-counter :init-num="10" @clicknum="handleClickNum" />
-    <click-counter :init-num="20" @clicknum="handleClickNum" />
-    <click-counter :init-num="30" @clicknum="handleClickNum">
-      <!-- 填坑用... -->
-      <input type="checkbox" /> 禁用
-    </click-counter>
+  <div class="container">
+    <div>计数结果：{{count}}</div>
+    <a href="/pages/test1/main" class="navlink">进入计数器页面</a>
   </div>
 </template>
 
 <script>
-// 导入 click-counter 组件
-import ClickCounter from '@/components/click-counter'
+import globalStore from '../../stores/global-store'
 
 export default {
-  // 声明在当前组件下使用 counter-click 组件
-  components: { ClickCounter },
-
-  data() {
-    return {
-      msg: 'Hello'
-    }
-  },
-
-  methods: {
-    clickHandle() {
-      this.msg = 'Clicked!!!!!!'
-    },
-    handleClickNum(data) {
-      console.log('>>>>>>', data.num)
+  computed: {
+    count() {
+      return globalStore.state.count
     }
   }
 }
 </script>
 
 <style scoped>
-.message {
-  color: red;
-  padding: 10px;
-  text-align: center;
+.navlink {
+  text-decoration: underline;
 }
 </style>
